69a91bcf9af0593daacd9425710553c33fac982c
Wojciech Danilo <wojciech.danilo@gmail.com>
2018-01-12 01:00:04 +0100

    Better border scaling

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ class Logo {
   constructor(size=64, compatibleMode=true) {
     this.size           = size;
     this.compatibleMode = compatibleMode;
-    this.borderWidth    = 10 - Math.log2(Math.min(size,128));
+    var scaleStop = 128;
+    var scaleLog  = Math.log2(scaleStop);
+    this.borderWidth    = 10 - Math.pow(Math.log2(Math.min(size,128))/scaleLog,3)*scaleLog;
     this.topRadius      = 32;
-    this.borderOffset   = this.borderWidth-1;
+    this.borderOffset   = this.borderWidth;
     this.innerRadius    = this.topRadius - this.borderWidth - this.borderOffset;
     this.atomRadius     = this.innerRadius / 2;
     this.atomDiff       = 0;
@@ -21,7 +23,7 @@ class Logo {
     this.body = `<use ${this.ref}="#logo" fill="red"/>`
   }
 
-  generate(size=64, compatibleMode=true) {
+  generate() {
     return `
 <svg height="${this.size}" width="${this.size}">
   <defs>
@@ -68,38 +70,39 @@ class Logo {
 }
 
 class InAppLogo extends Logo {
-  constructor(size=64, compatibleMode=true) {
+  constructor(size, compatibleMode) {
     super(size,compatibleMode);
     this.body = `<use ${this.ref}="#logo" fill="#AE8035"/>`
   }
 }
 
 class AppLogo extends Logo {
-  constructor(size=64, compatibleMode=true) {
+  constructor(size, compatibleMode) {
     super(size,compatibleMode);
 
     var border = 4
     var scale  = (64-2*border)/64
-    // this.size += 2*border
 
     this.defs = `
        <linearGradient id="cd" x1="0%" y1="0%" x2="0%" y2="100%" gradientUnits="userSpaceOnUse">
          <stop offset="0%"   style="stop-color:#ECAE67;stop-opacity:1" />
          <stop offset="100%" style="stop-color:#E2963B;stop-opacity:1" />
        </linearGradient>
-       <filter id="toShadow" x="0" y="0" width="200%" height="200%">
+       <filter id="toShadow" x="-50%" y="-50%" width="200%" height="200%">
          <feOffset result="offOut" in="SourceAlpha" dx="0" dy="2" />
          <feGaussianBlur result="blurOut" in="offOut" stdDeviation="1" />
        </filter>
+       <g id="txLogo" transform="translate(${border}, ${border})"><use ${this.ref}="#scalledLogo"/></g>
        <g id="scalledLogo" transform="scale(${scale})"><use ${this.ref}="#filledLogo"/></g>
-       <g id="filledLogo" transform="translate(${border}, ${border})">
+       <g id="filledLogo">
          <circle cx="32" cy="32" r="30" fill="#211F1A"/>
          <use ${this.ref}="#logo" fill="url(#cd)"/>
        </g>`;
     this.body = `
-        <use ${this.ref}="#scalledLogo" filter="url(#toShadow)" opacity="0.7"/>
-        <use ${this.ref}="#scalledLogo" />
-      `
+        <use ${this.ref}="#txLogo" filter="url(#toShadow)" opacity="0.7"/>
+        <use ${this.ref}="#txLogo"/>
+      `;
+
   }
 }
 
