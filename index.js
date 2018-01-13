02fc1e77f0b74a9e16ffa4e418daf448b8e0fdd8
Wojciech Danilo <wojciech.danilo@gmail.com>
2018-01-13 14:38:17 +0100

    Adding Luna logo with text

--- index.js
+++ index.js
@@ -1,4 +1,123 @@
+//
+// class SVG {
+//   constructor(){
+//     this.defs   = [];
+//     this.bodies = [];
+//   }
+//
+//   render() {
+//
+//   }
+// }
 
+
+class Text {
+  constructor(size=64, compatibleMode=true, xoff=0) {
+    this.size           = size;
+    this.compatibleMode = compatibleMode;
+    this.borderMax      = 10
+    this.borderSpread   = 0
+    this.xoff           = xoff
+    this.init();
+  }
+
+  init() {
+    var scaleStop = 128;
+    var scaleLog  = Math.log2(scaleStop);
+    this.borderWidth    = this.borderMax - Math.pow(Math.log2(Math.min(this.size,128))/scaleLog,3)*scaleLog;
+    this.scale          = this.size/64;
+
+    this.d = this.borderWidth;
+    this.height = 64;
+    this.r = this.height/3.5;
+    this.baseHeight = this.r*2;
+
+    this.ww = 240;
+    this.hh = this.size;
+
+    this.spacing = 10;
+
+    if (this.compatibleMode) {this.ref = "xlink:href"}
+    else                     {this.ref = "href"}
+
+    this.defs = ''
+    this.body = `<use ${this.ref}="#text" fill="#AE8035"/>`
+  }
+
+  generate() {
+    var widthOf_l    = this.d + this.spacing
+    var widthOf_lu   = widthOf_l   + 2*this.r + this.spacing
+    var widthOf_lun  = widthOf_lu  + 2*this.r + this.spacing
+    var widthOf_luna = widthOf_lun + 2*this.r + this.spacing
+    var docWidth = (widthOf_luna + this.xoff - this.spacing) * this.scale
+
+    return `
+<svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" height="${this.hh}" width="${docWidth}" viewBox="0 0 ${docWidth} ${this.hh}">
+  <defs>
+
+    <rect id="bg" width="${this.ww}" height="${this.hh}" fill="white"/>
+
+
+    <circle id="dotT" r="${this.d/2}" cx="${this.d/2}" cy="${this.d/2}"/>
+    <circle id="dotB" r="${this.d/2}" cx="${this.d/2}" cy="-${this.d/2}"/>
+    <circle id="dotM" r="${this.d/2}" cx="${this.d/2}" cy="${this.d/2 + this.height - this.baseHeight}"/>
+
+    <g id="letter_l">
+      <use ${this.ref}="#dotT"/>
+      <rect y="${this.d/2}" width="${this.d}" height="${this.height-this.d}"/>
+      <use transform="translate(0,${this.height})" ${this.ref}="#dotB"/>
+    </g>
+
+    <mask id="letter_u_mask">
+      <use ${this.ref}="#bg"/>
+      <circle cx="${this.r}" cy="${this.height-this.r}" r="${this.r-this.d}" fill="black"/>
+      <rect width="${this.r*2}" height="${this.height-this.r-1}" fill="black"/>
+    </mask>
+
+    <rect id="letter_u_side" y="${this.height-this.baseHeight+this.d/2}" width="${this.d}" height="${this.baseHeight-this.r-this.d/2}"/>
+
+    <g id="letter_u">
+      <use ${this.ref}="#dotM"/>
+      <use ${this.ref}="#dotM" transform="translate(${this.r*2-this.d})"/>
+      <circle cx="${this.r}" cy="${this.height-this.r}" r="${this.r}" mask="url(#letter_u_mask)"/>
+      <use ${this.ref}="#letter_u_side"/>
+      <use ${this.ref}="#letter_u_side" transform="translate(${this.r*2-this.d})"/>
+    </g>
+
+    <g id="letter_n">
+      <use ${this.ref}="#letter_u" transform="rotate(180,${this.r},${this.height}) translate(0,${this.baseHeight})"/>
+    </g>
+
+    <mask id="letter_a_mask">
+      <use ${this.ref}="#bg"/>
+      <circle cx="${this.r}" cy="${this.height-this.r}" r="${this.r-this.d}" fill="black"/>
+      <rect y="${this.height-this.r}" x="${this.r}" width="${this.r-this.d}" height="${this.r-this.d}" fill="black"/>
+
+    </mask>
+
+    <g id="letter_a" mask="url(#letter_a_mask)">
+      <circle cx="${this.r}" cy="${this.height-this.r}" r="${this.r}"/>
+      <rect y="${this.height-this.r}" x="${this.r}" width="${this.r}" height="${this.r}"/>
+    </g>
+
+
+
+
+    <g id="text">
+       <use ${this.ref}="#letter_l"/>
+       <use ${this.ref}="#letter_u" transform="translate(${widthOf_l})"/>
+       <use ${this.ref}="#letter_n" transform="translate(${widthOf_lu})"/>
+       <use ${this.ref}="#letter_a" transform="translate(${widthOf_lun})"/>
+    </g>
+
+    ${this.defs}
+
+    </defs>
+    <g transform="scale(${this.scale}) translate(${this.xoff})">${this.body}</g>
+</svg>
+`
+  }
+}
 
 
 class Logo {
@@ -171,9 +290,12 @@ class WhiteBackgroundLogo extends Logo {
 
 fastGenerate = (cons) => (...args) => new cons(...args).generate()
 
-exports.generateLogo             = fastGenerate(Logo);
-exports.generateAppLogo          = fastGenerate(AppLogo);
-exports.generateInAppLogo        = fastGenerate(InAppLogo);
-exports.generateMinimalBlackLogo = fastGenerate(MinimalBlackLogo);
+exports.generateLogo                = fastGenerate(Logo);
+exports.generateAppLogo             = fastGenerate(AppLogo);
+exports.generateInAppLogo           = fastGenerate(InAppLogo);
+exports.generateMinimalBlackLogo    = fastGenerate(MinimalBlackLogo);
 exports.generateBlackBackgroundLogo = fastGenerate(BlackBackgroundLogo);
 exports.generateWhiteBackgroundLogo = fastGenerate(WhiteBackgroundLogo);
+exports.generateInAppLogoWithText   = (size, mode) => exports.generateInAppLogo(size, mode)+exports.generateText(size,mode,20);
+
+exports.generateText             = fastGenerate(Text);
